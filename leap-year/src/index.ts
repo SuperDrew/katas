b6c238532df6a1b18454df21d926a5374e8fee43
Andrew Fraser <a.vector@gmail.com>
2022-08-01 16:23:52 +0200

    extract method

--- leap-year/src/index.ts
+++ leap-year/src/index.ts
@@ -1,7 +1,11 @@
+const isDivisibleBy = (number: number, divisor: number) => {
+  return number % divisor === 0;
+};
+
 export const isLeapYear = (year: number) => {
-  if (year % 400 === 0) {
+  if (isDivisibleBy(year, 400)) {
     return true;
-  } else if (year % 4 === 0 && year % 100 !== 0) {
+  } else if (isDivisibleBy(year, 4) && !isDivisibleBy(year, 100)) {
     return true;
   } else return false;
 };
