1fe3ea51310f59087f318922e20830697454c07a
Andrew Fraser <a.vector@gmail.com>
2022-08-03 10:53:38 +0200

    adding moar test cases for arrays with 3 values

--- chop/attempt1/src/index.spec.ts
+++ chop/attempt1/src/index.spec.ts
@@ -18,6 +18,11 @@ describe("Binary Chop", () => {
     it.each([
       [0, 1, [1, 3, 5]],
       [1, 3, [1, 3, 5]],
+      [2, 5, [1, 3, 5]],
+      [-1, 0, [1, 3, 5]],
+      [-1, 2, [1, 3, 5]],
+      [-1, 4, [1, 3, 5]],
+      [-1, 6, [1, 3, 5]],
     ])(
       "it should expect %s, when searching for %s in array %s",
       (expectedResponse, searchTarget, searchArray) => {
