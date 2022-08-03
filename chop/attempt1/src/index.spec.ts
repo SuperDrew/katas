34c83bdaa9e65a8eb802976be7d1f4d9b9de69b2
Andrew Fraser <a.vector@gmail.com>
2022-08-03 10:52:49 +0200

    adding noddy implementation for arrays with 3 values oh god this is horrid -> but will get better

--- chop/attempt1/src/index.spec.ts
+++ chop/attempt1/src/index.spec.ts
@@ -1,14 +1,28 @@
 import { chop } from ".";
 
 describe("Binary Chop", () => {
-  it.each([
-    [-1, 3, []],
-    [-1, 3, [1]],
-    [0, 1, [1]],
-  ])(
-    "it should expect %s, when searching for %s in array %s",
-    (expectedResponse, searchTarget, searchArray) => {
-      expect(chop(searchTarget, searchArray)).toBe(expectedResponse);
-    }
-  );
+  describe("Empty and Single value arrays", () => {
+    it.each([
+      [-1, 3, []],
+      [-1, 3, [1]],
+      [0, 1, [1]],
+    ])(
+      "it should expect %s, when searching for %s in array %s",
+      (expectedResponse, searchTarget, searchArray) => {
+        expect(chop(searchTarget, searchArray)).toBe(expectedResponse);
+      }
+    );
+  });
+
+  describe("Arrays with 3 values", () => {
+    it.each([
+      [0, 1, [1, 3, 5]],
+      [1, 3, [1, 3, 5]],
+    ])(
+      "it should expect %s, when searching for %s in array %s",
+      (expectedResponse, searchTarget, searchArray) => {
+        expect(chop(searchTarget, searchArray)).toBe(expectedResponse);
+      }
+    );
+  });
 });
--- chop/attempt1/src/index.ts
+++ chop/attempt1/src/index.ts
@@ -4,5 +4,15 @@ const isArrayEmpty = (searchArray: number[]) =>
 export const chop = (searchTarget: number, searchArray: number[]) => {
   if (isArrayEmpty(searchArray)) return -1;
   if (searchArray.length === 1 && searchArray[0] === searchTarget) return 0;
+  const middleOfArray = Math.floor(searchArray.length / 2);
+  const middleArrayValue = searchArray[middleOfArray];
+  if (middleArrayValue === searchTarget) return middleOfArray;
+  else if (middleArrayValue < searchTarget) {
+    if (searchArray[middleOfArray + 1] === searchTarget)
+      return middleOfArray + 1;
+  } else {
+    if (searchArray[middleOfArray - 1] === searchTarget)
+      return middleOfArray - 1;
+  }
   return -1;
 };
