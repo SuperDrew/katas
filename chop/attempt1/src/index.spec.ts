d54252ca61fb94963ac77202fc1fd51c948b647b
Andrew Fraser <a.vector@gmail.com>
2022-08-04 14:21:54 +0200

    extract getMiddleOfSet

--- chop/attempt1/src/index.spec.ts
+++ chop/attempt1/src/index.spec.ts
@@ -1,4 +1,4 @@
-import { chop } from ".";
+import { chop, getMiddleOfSubset } from ".";
 
 describe("Binary Chop", () => {
   describe("Empty and Single value arrays", () => {
@@ -29,5 +29,29 @@ describe("Binary Chop", () => {
         expect(chop(searchTarget, searchArray)).toBe(expectedResponse);
       }
     );
+  });
+
+  describe("DEBUG: ", () => {
+    it.each([[0, 1, [1, 3, 5]]])(
+      "DEBUG: it should expect %s, when searching for %s in array %s",
+      (expectedResponse, searchTarget, searchArray) => {
+        expect(chop(searchTarget, searchArray)).toBe(expectedResponse);
+      }
+    );
+  });
+});
+
+describe("Finding the middle of the subset", () => {
+  it("should return 1 for 3 elements", () => {
+    expect(getMiddleOfSubset(2, 0)).toBe(1);
+  });
+  it("should return 0 for 1 elements", () => {
+    expect(getMiddleOfSubset(0, 0)).toBe(0);
+  });
+  it("should return 1 for 4 elements", () => {
+    expect(getMiddleOfSubset(3, 0)).toBe(1);
+  });
+  it("should return 2 for 5 elements", () => {
+    expect(getMiddleOfSubset(4, 0)).toBe(2);
   });
 });
--- chop/attempt1/src/index.ts
+++ chop/attempt1/src/index.ts
@@ -1,18 +1,31 @@
 const isArrayEmpty = (searchArray: number[]) =>
   !Array.isArray(searchArray) || searchArray.length === 0;
 
+export const getMiddleOfSubset = (endIndex: number, startIndex: number) =>
+  Math.floor((endIndex - startIndex) / 2) + startIndex;
+
+const searchSubSet = (
+  searchTarget: number,
+  startIndex: number,
+  endIndex: number,
+  searchArray: number[]
+): number => {
+  const middleOfSubset = getMiddleOfSubset(endIndex, startIndex);
+  const middleSubsetValue = searchArray[middleOfSubset];
+  if (middleSubsetValue === searchTarget) return middleOfSubset;
+  if (middleSubsetValue < searchTarget) {
+    return searchSubSet(searchTarget, middleOfSubset, endIndex, searchArray);
+  }
+  if (middleSubsetValue > searchTarget) {
+    return searchSubSet(searchTarget, startIndex, middleOfSubset, searchArray);
+  }
+  return -1;
+};
+
 export const chop = (searchTarget: number, searchArray: number[]) => {
   if (isArrayEmpty(searchArray)) return -1;
   if (searchArray.length === 1 && searchArray[0] === searchTarget) return 0;
-  const middleOfArray = Math.floor(searchArray.length / 2);
-  const middleArrayValue = searchArray[middleOfArray];
-  if (middleArrayValue === searchTarget) return middleOfArray;
-  else if (middleArrayValue < searchTarget) {
-    if (searchArray[middleOfArray + 1] === searchTarget)
-      return middleOfArray + 1;
-  } else {
-    if (searchArray[middleOfArray - 1] === searchTarget)
-      return middleOfArray - 1;
-  }
+  else return -1;
+  return searchSubSet(searchTarget, 0, searchArray.length - 1, searchArray);
   return -1;
 };
