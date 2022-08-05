4020811dce1fa4e7ceb1ce73072b3ecfe4fa2512
Andrew Fraser <a.vector@gmail.com>
2022-08-05 20:23:25 +0200

    add property based checks, found an error when a subset is has 2 elements and the search is smaller than the value in index 1!

--- chop/attempt1/src/index.spec.ts
+++ chop/attempt1/src/index.spec.ts
@@ -1,3 +1,5 @@
+import * as fc from "fast-check";
+
 import { chop, getMiddleOfSubsetIndex } from ".";
 
 describe("Binary Chop", () => {
@@ -51,7 +53,7 @@ describe("Binary Chop", () => {
   });
 
   describe("DEBUG: ", () => {
-    it.each([[2, 5, [1, 3, 5]]])(
+    it.each([[-1, 0, [1, 3]]])(
       "DEBUG: it should expect %s, when searching for %s in array %s",
       (expectedResponse, searchTarget, searchArray) => {
         expect(chop(searchTarget, searchArray)).toBe(expectedResponse);
@@ -72,5 +74,32 @@ describe("Finding the middle of the subset", () => {
   });
   it("should return 2 for 5 elements", () => {
     expect(getMiddleOfSubsetIndex(4, 0)).toBe(2);
+  });
+});
+
+describe("Property based tests", () => {
+  it("should return the index of a number in a sorted array", () => {
+    fc.assert(
+      fc.property(fc.array(fc.integer(), { minLength: 1 }), (data) => {
+        const sortedArray = data.sort((a, b) => a - b);
+        const expectedIndex = Math.floor(Math.random() * sortedArray.length);
+        const searchTarget = sortedArray[expectedIndex];
+        expect(chop(searchTarget, sortedArray)).toBe(expectedIndex);
+      }),
+      { verbose: true }
+    );
+  });
+
+  it("should return -1 for a number not in a sorted array", () => {
+    fc.assert(
+      fc.property(fc.uniqueArray(fc.integer(), { minLength: 2 }), (data) => {
+        const sortedArray = data.sort((a, b) => a - b);
+        const indexToRemove = Math.floor(Math.random() * sortedArray.length);
+        const searchTarget = sortedArray[indexToRemove];
+        sortedArray.splice(indexToRemove, 1);
+        expect(chop(searchTarget, sortedArray)).toBe(-1);
+      }),
+      { verbose: true, seed: -68503183, path: "3", endOnFailure: true }
+    );
   });
 });
