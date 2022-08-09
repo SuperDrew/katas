57ded2ecc7c6e83bb53d0eaad67dbc19a4e1e5d9
Andrew Fraser <a.vector@gmail.com>
2022-08-09 19:36:41 +0200

    add 2 element arrays fast-check for the win

--- bubble/attempt1/src/index.spec.ts
+++ bubble/attempt1/src/index.spec.ts
@@ -1,9 +1,51 @@
+import * as fc from "fast-check";
+
 import { bubble } from "./index";
 
 describe("bubble", () => {
   describe("array with no elements", () => {
     it("should return the empty array", () => {
       expect(bubble([])).toStrictEqual([]);
+    });
+  });
+
+  describe("array with 1 elements", () => {
+    it("should return the same array", () => {
+      fc.property(fc.integer(), (num) => {
+        expect(bubble([num])).toStrictEqual([num]);
+      });
+    });
+  });
+
+  describe("array with 2 elements", () => {
+    describe(" that are already sorted", () => {
+      it("should return a sorted array", () => {
+        fc.assert(
+          fc.property(
+            fc
+              .tuple(fc.integer(), fc.integer())
+              .map(([a, b]) => (a <= b ? [a, b] : [b, a])),
+            ([a, b]) => {
+              expect([a, b]).toStrictEqual([a, b]);
+            }
+          )
+        );
+      });
+    });
+    describe(" that are not sorted", () => {
+      it("should return a sorted array", () => {
+        fc.assert(
+          fc.property(
+            fc
+              .tuple(fc.integer(), fc.integer())
+              .map(([a, b]) => (a <= b ? [a, b] : [b, a])),
+            ([a, b]) => {
+              expect(bubble([b, a])).toStrictEqual([a, b]);
+            }
+          ),
+          { verbose: true }
+        );
+      });
     });
   });
 });
--- bubble/attempt1/src/index.ts
+++ bubble/attempt1/src/index.ts
@@ -1,3 +1,7 @@
 export function bubble(data: number[]): number[] {
+  if (data.length === 0) return data;
+  if (data[0] > data[1]) {
+    return [data[1], data[0]];
+  }
   return data;
 }
