31801f5e3dd00088507c64449193a1fb8dc47660
Andrew Fraser <a.vector@gmail.com>
2022-08-12 12:48:00 +0200

    refactor extract method

--- bubble/attempt1/src/index.ts
+++ bubble/attempt1/src/index.ts
@@ -1,12 +1,16 @@
+const swapElements = (data: number[], i: number) => {
+  const temp = data[i];
+  data[i] = data[i + 1];
+  data[i + 1] = temp;
+};
+
 export const bubble = (data: number[]): number[] => {
   let done = false;
   while (!done) {
     let swappedAnElement = false;
     for (let i = 0; i < data.length - 1; i++) {
       if (data[i] > data[i + 1]) {
-        const temp = data[i];
-        data[i] = data[i + 1];
-        data[i + 1] = temp;
+        swapElements(data, i);
         swappedAnElement = true;
       }
     }
